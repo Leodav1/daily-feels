b41d7d946096b1a4f84d22b6807ba23324f981e3
leodav1 <ldmujica@gmail.com>
2024-05-19 19:32:41 +0200

    add chatgpt

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { LoginPage } from './components/pages/LoginPage';
 import { DashboardPage } from './components/pages/DashboardPage';
 import { JournalPage } from './components/pages/JournalPage';
-import { SettingsPage } from './components/pages/SettingsPage';
 import { Header } from './components/layout/Header';
 import './assets/styles/main.scss';
 
@@ -12,10 +10,8 @@ function App() {
       <Header /> 
       <Routes>
         <Route path="/" element={<Navigate replace to="/dashboard" />} />
-        <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/journal" element={<JournalPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
       </Routes>
     </Router>
   );
